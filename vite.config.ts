f8d15fafe3b7e75e3dcd0aa7961479f5cd4d72c9
unknown <talktogbenga@gmail.com>
2025-05-17 17:43:51 +0100

    Create  a simple Extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  publicDir: "public",
+});
